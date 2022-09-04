39ba2e9414bddc368a54cd82ab4b445196ffa1e1
Light2288 <davide.aliti@gmail.com>
2022-09-04 23:51:26 +0200

    Initial Commit

---
+++ Slot Machine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slot Machine
+//
+//  Created by Davide Aliti on 04/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
